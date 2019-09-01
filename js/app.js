2c0326d3bc1019898aed6f709eeef3de53038801
Anna <anna.sollazzo@shaw.ca>
2019-09-01 15:47:59 -0600

    modals and asides

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 //global config to make gh pages/local dev easier
-var prefix = './'
+var prefix = '/rcf-comediens/'
 
 //d3 integration taken from http://www.ng-newsletter.com.s3-website-us-east-1.amazonaws.com/posts/d3-on-angular.html
 angular.module('d3', [])
@@ -114,7 +114,6 @@ app.controller('ctrl', function($scope, $window, $uibModal, $translate) {
   }
 
   $scope.closeModal = function() {
-    console.log("fired")
     $scope.model.modalInstance.close();
   }
 
